2c8845ea5eff276f5de700fab028532c0d333907
Dan Smith <bulk70@gmail.com>
2015-11-24 21:48:08 +0000

    Heavily reworked FF event/communication code to make it more betterer

--- data/app/platform.firefox.js
+++ data/app/platform.firefox.js
@@ -6,48 +6,26 @@ YTG.platform = (function (YTG, platform) {
 
 	platform.getStorageItem = function(key, callback)
 	{
-        var uuid = platform.uuid();
+        var callbackName = 'storageGetCallback' + Math.random();
 
-        if (typeof(callback) == 'function')
-        {
-            platform.callbacks[uuid] = callback;
-        }
+        // Listen for the callback from firefox.js
+        self.port.once(callbackName, callback);
 
-        self.port.emit("getStorage", {uuid: uuid});
+        self.port.emit("getStorage", callbackName);
 	};
 
 	platform.setStorageItem = function(key, value, callback)
 	{
-        var uuid = platform.uuid();
+        var payload = {key: key, value: value, callbackName: null};
 
 		if (typeof(callback) == 'function')
         {
-            platform.callbacks[uuid] = callback;
+            payload.callbackName = 'storageSetCallback' + Math.random();
+            self.port.once(payload.callbackName, callback);
         }
 
-		self.port.emit("setStorage", {key: key, value: value, uuid: uuid});
+		self.port.emit("setStorage", payload);
 	};
-
-    // http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-    platform.uuid = function()
-    {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = crypto.getRandomValues(new Uint8Array(1))[0]%16|0, v = c == 'x' ? r : (r&0x3|0x8);
-            return v.toString(16);
-        });
-    };
 
 	return platform;
 }(YTG, YTG.platform || {}));
-
-self.port.on('executeCallback', function(payload)
-{
-    console.log(payload);
-    if (YTG.platform.callbacks[payload.uuid])
-    {
-        YTG.platform.callbacks[payload.uuid](payload.data);
-
-        // Remove the callback.
-        YTG.platform.callbacks.splice(payload.uuid, 1);
-    }
-});
